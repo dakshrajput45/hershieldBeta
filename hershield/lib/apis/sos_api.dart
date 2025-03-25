1060a7e66dffe8c098f89648b994dc9fe156eb12
dakshrajput45 <dakshr050@gmail.com>
2025-03-26 02:34:51 +0530

    find by user path added

--- hershield/lib/apis/sos_api.dart
+++ hershield/lib/apis/sos_api.dart
@@ -1,8 +1,13 @@
+import 'package:hershield/helper/api_paths.dart';
 import 'package:hershield/helper/log.dart';
+import 'package:http/http.dart' as http;
 
 class SosApi {
-  Future<void> findNearByUser() async {
-    try {} catch (error) {
+  Future<void> findNearByUser({required String userId}) async {
+    try {
+      final response =
+          http.get(Uri.parse("${ApiPaths.findNearByUserPath}?uid=$userId)"));
+    } catch (error) {
       hsLog('Error finding near by user: $error');
     }
   }
--- hershield/lib/helper/api_paths.dart
+++ hershield/lib/helper/api_paths.dart
@@ -1,4 +1,5 @@
 class ApiPaths {
   static String hostUrl = "http://192.168.1.34:3000/api/hershield";
   static String updateLocationPath = '$hostUrl/update-location';
+  static String findNearByUserPath = '$hostUrl/find-nearby-users';
 }
