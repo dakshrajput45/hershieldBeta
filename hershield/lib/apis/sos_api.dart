216dad55edb4071f824fe486999b96d2148a7b93
dakshAvent <daksh.rajput@aventiq.ai>
2025-03-26 02:28:33 +0530

    minor fix

--- hershield/lib/apis/sos_api.dart
+++ hershield/lib/apis/sos_api.dart
@@ -1,5 +1,9 @@
+import 'package:hershield/helper/log.dart';
+
 class SosApi {
   Future<void> findNearByUser() async {
-    
+    try {} catch (error) {
+      hsLog('Error finding near by user: $error');
+    }
   }
 }
