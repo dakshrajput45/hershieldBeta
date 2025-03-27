2e814c659f8b9c12b1ba31725597f63e90d5b27d
dakshrajput45 <dakshr050@gmail.com>
2025-03-28 05:23:55 +0530

    updated api path with vercel url

--- hershield/lib/helper/api_paths.dart
+++ hershield/lib/helper/api_paths.dart
@@ -1,5 +1,5 @@
 class ApiPaths {
-  static String hostUrl = "http://192.168.1.36:3000/api/hershield";
+  static String hostUrl = "https://hershield-beta.vercel.app/api/hershield";
   static String updateLocationPath = '$hostUrl/update-location';
   static String findNearByUserPath = '$hostUrl/find-nearby-users';
 }
